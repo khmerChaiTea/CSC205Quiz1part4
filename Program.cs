154da22aff332b67be378d507fc66bc8cd78f62c
pchai <prempreychhay@gmail.com>
2024-07-14 08:37:31 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Change Using to using and add ';'
+// Change Systems to System
+using System;
+
+namespace CSC205Quiz01
+{
+    // Change Class to class
+    class program
+    {
+        // Change main to Main
+        // Change void static to static void
+        // Change '()' for method main to '{}' and remove ';'
+        // Parameter of Main method missing '[]' after string
+        static void Main(string[] args)
+        {
+            // Console.Writeline to Console.WriteLine (capital the 'L'
+            // Missing ';' at the end
+            Console.WriteLine("I just removed all the bugs!");
+            Console.ReadKey();
+        }
+    }
+}
